b1c745a3c62ec6192e2f90d2ae8c48ab40d66f8d
Dodie Moore <dmoore@everquote.com>
2016-08-16 08:33:11 -0400

    Install Material UI and embed Robot fonts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import AppBar from 'material-ui/AppBar';
 import './App.css';
 
+injectTapEventPlugin();
+
 class App extends Component {
+  handleTouchTap = (e) => {
+    console.log('tapped', e)
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <AppBar
+        title="Bia"
+        onTouchTap={ this.handleTouchTap } />
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './App';
 import './index.css';
 
+const Index = () => (
+  <MuiThemeProvider muiTheme={ getMuiTheme(darkBaseTheme) }>
+    <App />
+  </MuiThemeProvider>
+);
+
 ReactDOM.render(
-  <App />,
+  <Index />,
   document.getElementById('root')
 );
